9fb3ea447e0ae415f219a6e2f16004e5bf4e0c01
Morre Thierry <thierry.morre@cognitic.be>
2022-08-24 14:12:17 +0200

    Ajoutez des fichiers projet.

---
+++ SortedCollectionSample/DataViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SortedCollectionSample
+{
+    public class DataViewModel
+    {
+        public string Text { get; init; }
+        public int Value { get; init; }
+        public DataViewModel(string text, int value)
+        {
+            Text = text;
+            Value = value;
+        }
+    }
+}
---
+++ SortedCollectionSample/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SortedCollectionSample
+{
+    public class MainViewModel : ObservableObject
+    {
+        private int _value;
+        private string? _text;
+
+        private ICommand? _addCommand;
+
+        private ObservableCollection<DataViewModel>? _items;
+
+        public ObservableCollection<DataViewModel> Items
+        {
+            get
+            {
+                return _items ??= new ObservableCollection<DataViewModel>();
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                SetProperty(ref _value, value);
+            }
+        }
+
+        public string? Text
+        {
+            get
+            {
+                return _text;
+            }
+
+            set
+            {
+                SetProperty(ref _text, value);
+            }
+        }
+
+        public ICommand AddCommand
+        {
+            get
+            {
+                return _addCommand ??= new RelayCommand(() => Items.Add(new DataViewModel(Text!, Value)), () => Text is not null);
+            }
+        }
+
+        public MainViewModel()
+        {
+            Items.Add(new DataViewModel("Toto", 1));
+            Items.Add(new DataViewModel("Ben", 1));
+            Items.Add(new DataViewModel("Alphonse", 2));
+        }
+
+        
+    }
+}
